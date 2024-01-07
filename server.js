5d7a2e03008cc9dd0d990300a2bced3b6b9f7391
kanakadurga1999 <kanakadurgapulikkal@gmail.com>
2024-01-07 21:17:15 +0530

    controllers

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const {logger} = require('./middleware/logger')
+const {logger,logEvents} = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions= require('./config/corsOptions')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 const PORT= process.env.PORT || 3500
 
 console.log(process.env.NODE_ENV)
 
+connectDB()
 app.use(logger)
 app.use(cors(corsOptions))
 app.use(express.json())
@@ -34,4 +37,14 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, ()=>console.log(`SERVER RUNNING ON PORT ${PORT}`))
+// app.listen(PORT, ()=>console.log(`SERVER RUNNING ON PORT ${PORT}`))
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
